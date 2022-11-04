546246631972d7304b1a2c1855dc9cb63eca2b34
Josh <joshcolemitchell@gmail.com>
2022-11-04 10:03:38 +0000

    found issue in daily

--- inflammation/models.py
+++ inflammation/models.py
@@ -41,3 +41,7 @@ def daily_min(data):
     """
     return np.min(data, axis=0)
 
+def patient_normalise(data):
+    """Normalise patient data from a 2D inflammation data array."""
+    max_data = np.max(data, axis=0)
+    return data / max_data[:, np.newaxis]
